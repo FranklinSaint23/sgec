f311c57f35e338987d37a7dc4d8854cbe242f912
Franklin Njiepi <franklinnjiepi07@gmail.com>
2026-06-18 21:52:43 +0100

    frontend: mise à jour URLs vers production Render Remplace 192.168.43.83:8000 par https://sgec-api.onrender.com dans api.js, SignupPage.jsx (register + login).

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.43.83:8000/api',
+  baseURL: 'https://sgec-api.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
